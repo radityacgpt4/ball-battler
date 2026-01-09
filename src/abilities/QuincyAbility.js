d7c1660d5122cf8619318e563e1c02affd0349c0
radityacgpt4 <161792338+radityacgpt4@users.noreply.github.com>
2026-01-09 15:00:57 +0700

    Update QuincyAbility.js

--- src/abilities/QuincyAbility.js
+++ src/abilities/QuincyAbility.js
@@ -173,7 +173,7 @@ export class QuincyDefAbility extends Ability {
         this.blinkDistance = config.blinkDistance || 150;
         this.trapDuration = config.trapDuration || 180;
         this.trapStunDuration = config.trapStunDuration || 30;
-        this.trapRadius = config.trapRadius || 7;
+        this.trapRadius = config.trapRadius || 1;
     }
 
     update(fighter, context) {
